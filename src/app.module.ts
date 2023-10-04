5d204c3138fe51031ad80f6e444a7b171107892f
tamyres <tamyrescaroline.oliveira@gmail.com>
2023-10-04 17:55:13 -0300

    feat: import prisma and visitors module in app

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { VisitorsModule } from './visitors/visitors.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, VisitorsModule],
   controllers: [AppController],
   providers: [AppService],
 })
