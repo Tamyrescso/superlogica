bfde23bb06769c2a3b595eab695b8d9e08621646
tamyres <tamyrescaroline.oliveira@gmail.com>
2023-10-05 17:20:51 -0300

    feat: add visitor log detail entity

--- src/visitors-log/entities/visitors-log.entity.ts
+++ src/visitors-log/entities/visitors-log.entity.ts
@@ -1,21 +1,44 @@
-import { ApiProperty } from "@nestjs/swagger";
+import { ApiProperty } from '@nestjs/swagger';
+import { CondoEntity } from 'src/condos/entities/condo.entity';
+import { UnitEntity } from 'src/units/entities/unit.entity';
+import { VisitorEntity } from 'src/visitors/entities/visitor.entity';
 
-export class VisitorsLogEntity {
-    @ApiProperty()
-    id: number;
+export class VisitorLogEntity {
+  @ApiProperty()
+  id: number;
 
-    @ApiProperty()
-    visitors_id: number;
+  @ApiProperty()
+  visitors_id: number;
 
-    @ApiProperty()
-    condos_id: number;
+  @ApiProperty()
+  condos_id: number;
 
-    @ApiProperty()
-    units_id: number;
+  @ApiProperty()
+  units_id: number;
 
-    @ApiProperty()
-    entry: Date;
+  @ApiProperty()
+  entry: Date;
 
-    @ApiProperty()
-    exit: Date;
+  @ApiProperty()
+  exit: Date;
+}
+
+export class VisitorLogDetailEntity {
+  @ApiProperty()
+  id: number;
+
+  @ApiProperty()
+  visitors: VisitorEntity;
+
+  @ApiProperty()
+  condos: CondoEntity;
+
+  @ApiProperty()
+  units: UnitEntity;
+
+  @ApiProperty()
+  entry: Date;
+
+  @ApiProperty()
+  exit: Date;
 }
