011058e6767acb10fe388be088cd703f097a7858
tamyres <tamyrescaroline.oliveira@gmail.com>
2023-10-05 03:13:39 -0300

    feat: create condos service

---
+++ src/condos/condos.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class CondosService {
+  constructor(private prisma: PrismaService) {}
+  async findAll() {
+    const condos = await this.prisma.condos.findMany();
+
+    return {
+      "info": {
+        "name": "Sistema de controle de acesso",
+        "description": "Lista de condomínios"
+      },
+      "items": condos
+    }
+  }
+
+  async findOne(id: number) {
+    const condo = await this.prisma.condos.findUnique({ where: { id } });
+    if (!condo) {
+      throw new NotFoundException({
+        error: `The condominium with id ${id} does not exist`,
+      });
+    }
+    return condo;
+  }
+}
