6636f0237006b8e24fee8fd572042557b84c8185
tamyres <tamyrescaroline.oliveira@gmail.com>
2023-10-05 03:17:33 -0300

    feat: add imports in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { VisitorsModule } from './visitors/visitors.module';
+import { UnitsModule } from './units/units.module';
+import { CondosModule } from './condos/condos.module';
+import { VisitorsLogModule } from './visitors-log/visitors-log.module';
 
 @Module({
-  imports: [PrismaModule, VisitorsModule],
+  imports: [
+    PrismaModule,
+    VisitorsModule,
+    UnitsModule,
+    CondosModule,
+    VisitorsLogModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
