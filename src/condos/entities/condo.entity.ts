e9e5c02311164ffb79c5031319f3f0ec4087a122
tamyres <tamyrescaroline.oliveira@gmail.com>
2023-10-05 03:13:53 -0300

    feat: create condos entity

---
+++ src/condos/entities/condo.entity.ts
@@ -0,0 +1,25 @@
+import { ApiProperty } from "@nestjs/swagger";
+
+export class CondoEntity {
+    @ApiProperty()
+    id: number;
+
+    @ApiProperty()
+    name: string;
+}
+
+class Info {
+    @ApiProperty()
+    name: string;
+
+    @ApiProperty()
+    description: string;
+
+    @ApiProperty()
+    items: CondoEntity[];
+}
+
+export class CondosEntity {
+    @ApiProperty()
+    info: Info;
+}
