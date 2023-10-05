2dc3f7fd14adcb7318b79703c3572ca3e4ee2d98
tamyres <tamyrescaroline.oliveira@gmail.com>
2023-10-05 03:15:32 -0300

    feat: create visitors dto

---
+++ src/visitors-log/dto/create-visitors-log.dto.ts
@@ -0,0 +1,19 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsNumber } from "class-validator";
+
+export class CreateVisitorsLogDto {
+    @ApiProperty()
+    @IsNumber()
+    @IsNotEmpty()
+    visitors_id: number;
+
+    @ApiProperty()
+    @IsNumber()
+    @IsNotEmpty()
+    condos_id: number;
+
+    @ApiProperty()
+    @IsNumber()
+    @IsNotEmpty()
+    units_id: number;
+}
