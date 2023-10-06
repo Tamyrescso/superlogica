5966a3257465b7355e60225de4a32a226d6fa0bd
tamyres <tamyrescaroline.oliveira@gmail.com>
2023-10-06 02:08:36 -0300

    style: just lint

--- prisma/seeds/condos.seed.ts
+++ prisma/seeds/condos.seed.ts
@@ -2,26 +2,24 @@ import { PrismaClient } from '@prisma/client';
 import { faker } from '@faker-js/faker';
 import * as dotenv from 'dotenv';
 
-const prisma  = new PrismaClient();
+const prisma = new PrismaClient();
 
 const fakerCondo = (): any => ({
-    name: faker.company.name(),
+  name: faker.company.name(),
 });
 
 async function main() {
-const fakerRounds = 10;
-dotenv.config();
-console.log('Seeding Condos...');
-/// --------- Condos ---------------
-for (let i = 0; i < fakerRounds; i++) {
-await prisma.condos.create({ data: fakerCondo() });
+  const fakerRounds = 10;
+  dotenv.config();
+  console.log('Seeding Condos...');
+  /// --------- Condos ---------------
+  for (let i = 0; i < fakerRounds; i++) {
+    await prisma.condos.create({ data: fakerCondo() });
+  }
 }
-};
-
-  
 
 main()
-.catch((e) => console.error(e))
-.finally(async () => {
-await prisma.$disconnect();
-});
+  .catch((e) => console.error(e))
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
--- prisma/seeds/visitors.seed.ts
+++ prisma/seeds/visitors.seed.ts
@@ -2,27 +2,25 @@ import { PrismaClient } from '@prisma/client';
 import { faker } from '@faker-js/faker';
 import * as dotenv from 'dotenv';
 
-const prisma  = new PrismaClient();
+const prisma = new PrismaClient();
 
 const fakerVisitor = (): any => ({
-fullName: faker.person.fullName(),
-rg: faker.string.numeric(9),
+  fullName: faker.person.fullName(),
+  rg: faker.string.numeric(9),
 });
 
 async function main() {
-const fakerRounds = 10;
-dotenv.config();
-console.log('Seeding Visitors...');
-/// --------- Visitors ---------------
-for (let i = 0; i < fakerRounds; i++) {
-await prisma.visitors.create({ data: fakerVisitor() });
+  const fakerRounds = 10;
+  dotenv.config();
+  console.log('Seeding Visitors...');
+  /// --------- Visitors ---------------
+  for (let i = 0; i < fakerRounds; i++) {
+    await prisma.visitors.create({ data: fakerVisitor() });
+  }
 }
-};
-
-  
 
 main()
-.catch((e) => console.error(e))
-.finally(async () => {
-await prisma.$disconnect();
-});
+  .catch((e) => console.error(e))
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
